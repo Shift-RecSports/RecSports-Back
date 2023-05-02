91d1fb45b2c41c09735ae2b7d1de508cf89fb6cf
martnreh <martinbarrientos451@gmail.com>
2023-05-02 10:33:38 -0600

    .env git ignore

--- src/database.js
+++ src/database.js
@@ -1,16 +1,12 @@
-// import mysql from 'mysql';
-
-
 const mysql = require('mysql');
-
+require('dotenv').config();
 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'hapi-server',
-    password: 'Computadora5',
-    database: 'RecSports',
-
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 
