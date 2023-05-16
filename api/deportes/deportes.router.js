f40582fdee9a29eadd9cd2bedbcff9b1bc14b351
martnreh <martinbarrientos451@gmail.com>
2023-05-16 00:30:44 -0600

    upload images

--- api/deportes/deportes.router.js
+++ api/deportes/deportes.router.js
@@ -1,7 +1,50 @@
-const {getDeportes, createDeporte} = require("./deportes.controller.js");
-const router = require("express").Router();
+const { getDeportes, createDeporte } = require("./deportes.service");
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
-router.get("/", getDeportes);
-router.post("/", createDeporte);
+const router = express.Router();
+
+// Set storage engine for multer
+const storage = multer.diskStorage({
+  destination: "./images/deportes",
+  filename: function (req, file, callback) {
+    // Generate a unique filename
+    const uniqueName = `deporte_${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${path.extname(file.originalname)}`;
+    callback(null, uniqueName);
+  },
+});
+
+// Create upload instance
+const upload = multer({ storage: storage });
+
+router.get("/", (req, res) => {
+  getDeportes((err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    return res.json(results);
+  });
+});
+
+router.post("/", upload.single("imagen"), (req, res) => {
+  console.log(req.body);
+  const body = req.body;
+  const filename = req.file.filename; // Retrieve the filename from the request
+  createDeporte({ ...body, imagen: filename }, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        success: 0,
+        message: "Database connection error",
+      });
+    }
+
+    return res.status(200).json(results);
+  });
+});
 
 module.exports = router;
--- api/deportes/deportes.service.js
+++ api/deportes/deportes.service.js
@@ -1,31 +1,44 @@
-const pool = require("../../config/database")
-
+const pool = require("../../config/database");
+const shortid = require("shortid");
 
 module.exports = {
-    getDeportes: callBack => {
-        pool.query(
-            `SELECT * FROM DEPORTES`,
+  getDeportes: (callBack) => {
+    pool.query(`SELECT * FROM DEPORTES`, (error, results, fields) => {
+      if (error) {
+        callBack(error);
+      }
+      return callBack(null, results);
+    });
+  },
+  createDeporte: (data, callBack) => {
+    const id = shortid.generate(); // 9 digits id
+    pool.query(
+      `INSERT INTO Deportes (id, nombre, descripcion, materiales, imagen, duracion) VALUES (?,?,?,?,?,?);`,
+      [
+        id,
+        data.nombre,
+        data.descripcion,
+        data.materiales,
+        data.imagen,
+        data.duracion,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        } else {
+          pool.query(
+            `SELECT * FROM Deportes WHERE id = ?;`,
+            [id],
             (error, results, fields) => {
-                if (error) {
-                    callBack(error)
-                }
-                return callBack(null, results)
+              if (error) {
+                callBack(error);
+              } else {
+                return callBack(null, results[0]);
+              }
             }
-        )
-    },
-    createDeporte: (data, callBack) => {
-        pool.query(
-            `INSERT INTO Deportes (id, nombre, descripcion, materiales, imagen, duracion) VALUES (?,?,?,?,?,?);`,
-            ["efsefae", data.nombre, data.descripcion, data.materiales, data.imagen, data.duracion],
-            (error, results, fields) => {
-                if (error) {
-                    callBack(error)
-                }
-                return callBack(null, results[0])
-
-            }
-        )
-
-    }
-
-}
+          );
+        }
+      }
+    );
+  },
+};
