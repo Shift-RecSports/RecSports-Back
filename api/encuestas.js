162c510a60b2c72aa62b04898b3b7370397bad61
Sasha Moroz <aleksandr.shane@gmail.com>
2023-05-26 00:09:02 -0600

    Endpoints para Encuestas

---
+++ api/encuestas.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const router = express.Router();
+const client = require("../config/database");
+
+//GET all encuestas
+router.get("/", (req, res) => {
+    client.query(`SELECT *, TO_CHAR(fecha, 'YYYY-MM-DD') AS fecha FROM encuestas;`, (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({
+          message: "Error respuesta de servidor",
+        });
+      }
+    
+      return res.json(results.rows);
+    });
+  });
+
+//GET encuestas by Id
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    client.query(`SELECT *, TO_CHAR(fecha, 'YYYY-MM-DD') AS fecha FROM encuestas WHERE id = $1;`,[id], (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({
+          message: "Error respuesta de servidor",
+        });
+      }
+      if (results.rows.length === 0) {
+        return res.status(404).json({
+          message: "Gimnasio no encontrado",
+        });
+      }
+      return res.status(200).json(results.rows[0]);
+    });
+  });
+
+//POST new encuesta
+router.post("/", (req, res) => {
+    const body = req.body;
+    client.query(
+      `INSERT INTO encuestas (matricula, fecha, calificacion1, calificacion2, calificacion3, tema, comentario)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+       RETURNING *, TO_CHAR(fecha, 'YYYY-MM-DD') AS fecha;`,
+    [body.matricula, body.fecha, body.calificacion1, body.calificacion2, body.calificacion3, body.tema, body.comentario],
+      (error, results) => {
+          if (error) {
+            console.log(error);
+            return res.status(500).json({
+              message: "Error respuesta de servidor",
+            });
+          } else {
+            return res.status(200).json(results.rows[0]);
+          }
+        }
+      );
+  });
+
+
+//UPDATE encuesta
+router.put("/", (req, res) => {
+    const body = req.body;
+    client.query(
+      `UPDATE encuestas SET matricula = $1, fecha = TO_DATE($2, 'YYYY-MM-DD'), calificacion1 = $3, 
+      calificacion2 = $4, calificacion3 = $5, tema = $6, comentario = $7 WHERE id = $8 RETURNING *;`,
+      [body.matricula, body.fecha, body.calificacion1, body.calificacion2, body.calificacion3, body.tema, body.comentario, body.id],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: "Error responding from the server",
+          });
+        } else {
+          return res.status(200).json(results.rows[0]);
+        }
+      }
+    );
+    
+  });
+
+
+//DELETE registro gimnasio
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    client.query(`DELETE FROM Encuestas WHERE id = $1`,
+      [id], (error, results, fields) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: "Error conexion base de datos",
+          });
+        }
+        return res.status(200).json({});
+      });
+  });
+
+
+
+module.exports = router;
--- api/reservaciones.js
+++ api/reservaciones.js
@@ -93,7 +93,8 @@ router.get("/deporte=:deporte/fecha=:fecha", (req, res) => {
 router.post("/", (req, res) => {
   const body = req.body;
   client.query(
-    `INSERT INTO Reservaciones (hora_seleccionada, matricula_alumno, fecha, espacio, estatus) VALUES ($1, $2, $3, $4, $5) RETURNING *, TO_CHAR(fecha, 'YYYY-MM-DD') AS fecha;`,
+    `INSERT INTO Reservaciones (hora_seleccionada, matricula_alumno, fecha, espacio, estatus) VALUES ($1, $2, $3, $4, $5) 
+    RETURNING *, TO_CHAR(fecha, 'YYYY-MM-DD') AS fecha;`,
     [body.hora_seleccionada, body.matricula_alumno, body.fecha, body.espacio, body.estatus],
     (error, results) => {
       if (error) {
@@ -113,7 +114,8 @@ router.put("/", (req, res) => {
   const body = req.body;
   client.query(
     `UPDATE Reservaciones
-    SET hora_seleccionada = $1, matricula_alumno = $2, fecha = $3, espacio = $4, estatus = $5 WHERE id = $6 RETURNING *, TO_CHAR(fecha, 'YYYY-MM-DD') AS fecha;`,
+    SET hora_seleccionada = $1, matricula_alumno = $2, fecha = $3, espacio = $4, estatus = $5 WHERE id = $6 
+    RETURNING *, TO_CHAR(fecha, 'YYYY-MM-DD') AS fecha;`,
     [body.hora_seleccionada, body.matricula_alumno, body.fecha, body.espacio, body.estatus, body.id],
     (error, results) => {
       if (error) {
